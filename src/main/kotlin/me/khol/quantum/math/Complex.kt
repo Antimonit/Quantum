4de21c9d91e485394f4826dab225b15eef62fce4
David Khol <david@khol.me>
2019-09-19 06:44:17 +0800

    Added Complex number.

---
+++ src/main/kotlin/me/khol/quantum/math/Complex.kt
@@ -0,0 +1,32 @@
+package me.khol.quantum.math
+
+import kotlin.math.sqrt
+
+data class Complex(
+    val re: Double,
+    val im: Double
+) {
+
+    operator fun unaryPlus() = Complex(re, im)
+    operator fun unaryMinus() = Complex(-re, -im)
+
+    operator fun plus(s: Complex) = Complex(re + s.re, im + s.im)
+    operator fun minus(s: Complex) = Complex(re - s.re, im - s.im)
+    operator fun times(s: Complex) = Complex(re * s.re - im * s.im, re * s.im + im * s.re)
+    operator fun div(s: Complex) = Complex(re * s.re + im * s.im, re * s.im - im * s.re) / s.square
+
+    operator fun times(s: Double) = Complex(re * s, im * s)
+    operator fun div(s: Double) = Complex(re / s, im / s)
+
+    val reciprocal: Complex
+        get() = Complex(re / square, -im / square)
+
+    val conjugate: Complex
+        get() = Complex(re, -im)
+
+    val square: Double
+        get() = re * re + im * im
+
+    val module: Double
+        get() = sqrt(square)
+}

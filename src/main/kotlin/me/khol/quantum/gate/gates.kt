4d4aa8eeca8c20f6ec12e872c1b05b1ace69018b
David Khol <david@khol.me>
2019-09-19 07:40:36 +0800

    Added some basic gates.

---
+++ src/main/kotlin/me/khol/quantum/gate/gates.kt
@@ -0,0 +1,27 @@
+package me.khol.quantum.gate
+
+import me.khol.quantum.math.matrix.Complex22
+import me.khol.quantum.math.scalar.Complex.Companion.ONE
+import me.khol.quantum.math.scalar.Complex.Companion.ZERO
+import me.khol.quantum.math.scalar.Complex.Companion.I
+import me.khol.quantum.math.vector.Complex2
+
+val gateX = Complex22(
+    Complex2(ZERO, ONE),
+    Complex2(ONE, ZERO)
+)
+
+val gateY = Complex22(
+    Complex2(ZERO, -I),
+    Complex2(I, ZERO)
+)
+
+val gateZ = Complex22(
+    Complex2(ONE, ZERO),
+    Complex2(ZERO, -ONE)
+)
+
+val gateIdentity = Complex22(
+    Complex2(ONE, ZERO),
+    Complex2(ZERO, ONE)
+)

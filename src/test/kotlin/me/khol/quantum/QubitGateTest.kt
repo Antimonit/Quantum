e87f47e2e691d602292113a55e3cbf73ca136c36
David Khol <david@khol.me>
2019-09-25 15:46:20 +0800

    Added more tests for Ket and Bra.

--- src/test/kotlin/me/khol/quantum/QubitGateTest.kt
+++ src/test/kotlin/me/khol/quantum/QubitGateTest.kt
@@ -2,6 +2,7 @@ package me.khol.quantum
 
 import me.khol.quantum.Qubit.Companion.ONE
 import me.khol.quantum.Qubit.Companion.ZERO
+import me.khol.quantum.math.Complex
 import me.khol.quantum.math.Matrix
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
@@ -14,5 +15,19 @@ internal class QubitGateTest {
     fun ketsAndBras() {
         val result = ZERO.ket * ZERO.bra + ONE.ket * ONE.bra
         assertEquals(Matrix.identity(2), result)
+    }
+
+    @DisplayName("Equation maps to Zero qubit")
+    @Test
+    fun mapsToZeroQubit() {
+        val result = (ZERO.ket * ONE.bra) * ONE.ket
+        assertEquals(ZERO.ket, result)
+    }
+
+    @DisplayName("Equation maps to zero")
+    @Test
+    fun mapsToZero() {
+        val result = (ZERO.ket * ONE.bra) * ZERO.ket
+        assertEquals(Matrix(2, 1, Complex.ZERO, Complex.ZERO), result)
     }
 }

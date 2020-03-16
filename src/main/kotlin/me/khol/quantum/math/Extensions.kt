8da38da4ba4c75ae625af90c4f637e5ee9d631a1
David Khol <david@khol.me>
2020-03-16 09:34:20 +0100

    Added integer to binary digits extension.

--- src/main/kotlin/me/khol/quantum/math/Extensions.kt
+++ src/main/kotlin/me/khol/quantum/math/Extensions.kt
@@ -2,6 +2,17 @@ package me.khol.quantum.math
 
 import kotlin.math.pow
 
+/**
+ * [pow] extension on integers.
+ */
 infix fun Int.pow(exponent: Int): Int {
     return toDouble().pow(exponent).toInt()
+}
+
+/**
+ * Convert [this] integer to a list of binary digits where the first element of the list
+ * is the most significant digit of [this] number. Contains exactly [size] elements.
+ */
+fun Int.toBinaryDigits(size: Int): List<Int> {
+    return (size - 1 downTo 0).map { this shr it and 1 }
 }

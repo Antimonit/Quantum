4ddf5cfaba32ca39ecbc6fd6ff5e30fa09ad86d1
David Khol <david@khol.me>
2020-03-18 21:06:27 +0100

    Convert list of binary digits to an integer.

--- src/main/kotlin/me/khol/quantum/math/Extensions.kt
+++ src/main/kotlin/me/khol/quantum/math/Extensions.kt
@@ -11,8 +11,16 @@ infix fun Int.pow(exponent: Int): Int {
 
 /**
  * Convert [this] integer to a list of binary digits where the first element of the list
- * is the most significant digit of [this] number. Contains exactly [size] elements.
+ * is the most significant digit of the number. Contains exactly [size] elements.
  */
 fun Int.toBinaryDigits(size: Int): List<Int> {
     return (size - 1 downTo 0).map { this shr it and 1 }
+}
+
+/**
+ * Convert [this] list of binary digits to an integer where the first element of the list
+ * is the most significant digit of the number.
+ */
+fun List<Int>.fromBinaryDigits(): Int {
+    return fold(0) { acc, value -> (acc shl 1) + value}
 }
--- src/test/kotlin/me/khol/quantum/math/ExtensionsTest.kt
+++ src/test/kotlin/me/khol/quantum/math/ExtensionsTest.kt
@@ -13,6 +13,11 @@ internal class ExtensionsTest {
 
     @Test
     fun `Integer to a list of binary digits`() {
-        assertThat(13.toBinaryDigits(4), equalTo(listOf(1, 1, 0, 1)))
+        assertThat(0b1101.toBinaryDigits(4), equalTo(listOf(1, 1, 0, 1)))
+    }
+
+    @Test
+    fun `List of binary digits to an integer`() {
+        assertThat(listOf(1, 1, 0, 1).fromBinaryDigits(), equalTo(0b1101))
     }
 }

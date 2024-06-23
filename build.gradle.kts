8b64e91897ce2a230905aee2857cbce2dd3317dc
David Khol <david.khol@bucketplace.net>
2024-06-23 20:10:47 +0900

    Update gradle-maven-publish-plugin to 0.29.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,6 @@ buildscript {
     repositories {
         mavenCentral()
     }
-    dependencies {
-        classpath("com.vanniktech:gradle-maven-publish-plugin:0.16.0")
-    }
 }
 
 allprojects {
